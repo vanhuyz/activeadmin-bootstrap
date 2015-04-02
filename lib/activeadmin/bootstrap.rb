7bf85b12af9ede87841631546a43085b87cbafbb
Van Huy <huyvpq@gmail.com>
2015-04-03 01:49:43 +0900

    init gem

---
+++ lib/activeadmin/bootstrap.rb
@@ -0,0 +1,7 @@
+require "activeadmin/bootstrap/version"
+
+module Activeadmin
+  module Bootstrap
+    # Your code goes here...
+  end
+end
---
+++ lib/activeadmin/bootstrap/version.rb
@@ -0,0 +1,5 @@
+module Activeadmin
+  module Bootstrap
+    VERSION = "0.0.1"
+  end
+end
